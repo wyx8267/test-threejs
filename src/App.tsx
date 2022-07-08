dc294a599cab98928f044ad2a133a74fa02deadd
wangyuxin <369264448@qq.com>
2022-07-08 18:31:48 +0800

    earth

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import HelloCamera from './components/hello-camera';
 import HelloCanvas from './components/hello-canvas';
 import PreserveDrawingBuffer from './components/hello-canvas/preserve-drawing-buffer';
+import HelloEarth from './components/hello-earth';
 import HelloFakeShadow from './components/hello-fake-shadow';
 import HelloFog from './components/hello-fog';
 import HelloLight from './components/hello-light';
@@ -27,7 +28,8 @@ function App() {
     // <HelloRenderTarget />
     // <RenderingOnDemand />
     // <HelloCanvas />
-    <PreserveDrawingBuffer />
+    // <PreserveDrawingBuffer />
+    <HelloEarth />
   );
 }
 
