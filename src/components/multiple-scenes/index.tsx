449787ad68247dd49d5b42d8fb22803b2ee8e38f
wangyuxin <369264448@qq.com>
2022-08-12 18:07:53 +0800

    multiple-scene-2

--- src/components/multiple-scenes/index.tsx
+++ src/components/multiple-scenes/index.tsx
@@ -1,13 +1,15 @@
 import React, { useEffect, useRef } from "react";
 import * as Three from "three";
-import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+import { TrackballControls } from "three/examples/jsm/controls/TrackballControls.js";
 import "./index.scss";
+const sceneElements: any[] = [];
 
 type SceneInfo = {
   scene: Three.Scene;
   camera: Three.PerspectiveCamera;
-  elem: Element | null;
+  elem?: Element | null;
   mesh?: Three.Mesh;
+  controls?: TrackballControls;
 };
 
 function makeScene(elem: Element | null): SceneInfo {
@@ -17,12 +19,17 @@ function makeScene(elem: Element | null): SceneInfo {
   const camera = new Three.PerspectiveCamera(45, 2, 0.1, 5);
   camera.position.set(0, 1, 2);
   camera.lookAt(0, 0, 0);
+  scene.add(camera);
+
+  const controls = new TrackballControls(camera, elem as HTMLElement);
+  controls.noZoom = true;
+  controls.noPan = true;
 
   const light = new Three.DirectionalLight(0xffffff, 1);
   light.position.set(-1, 2, 4);
-  scene.add(light);
+  camera.add(light);
 
-  return { scene, camera, elem };
+  return { scene, camera, controls };
 }
 
 function setupScene1() {
@@ -48,7 +55,11 @@ function setupScene2() {
   return sceneInfo;
 }
 
-function renderSceneInfo(sceneInfo: SceneInfo, renderer: Three.WebGLRenderer, canvas: HTMLCanvasElement) {
+function renderSceneInfo(
+  sceneInfo: SceneInfo,
+  renderer: Three.WebGLRenderer,
+  canvas: HTMLCanvasElement
+) {
   const { scene, camera, elem } = sceneInfo;
 
   const { left, right, top, bottom, width, height } =
@@ -61,12 +72,12 @@ function renderSceneInfo(sceneInfo: SceneInfo, renderer: Three.WebGLRenderer, ca
     left > renderer.domElement.clientWidth;
   if (isOffScreen) return;
 
-  camera.aspect = width / height
-  camera.updateProjectionMatrix()
-  const positiveYUpBottom = canvas.height - bottom
-  renderer.setScissor(left, positiveYUpBottom, width, height)
-  renderer.setViewport(left, positiveYUpBottom, width, height)
-  renderer.render(scene, camera)
+  camera.aspect = width / height;
+  camera.updateProjectionMatrix();
+  const positiveYUpBottom = canvas.height - bottom;
+  renderer.setScissor(left, positiveYUpBottom, width, height);
+  renderer.setViewport(left, positiveYUpBottom, width, height);
+  renderer.render(scene, camera);
 }
 
 let flag = false;
@@ -81,24 +92,90 @@ const MultipleScenes: React.FC = () => {
     const canvas = canvasRef.current;
     const renderer = new Three.WebGLRenderer({ canvas });
 
-    const sceneInfo1 = setupScene1();
-    const sceneInfo2 = setupScene2();
+    // const sceneInfo1 = setupScene1();
+    // const sceneInfo2 = setupScene2();
+
+    const sceneInitFunctionsByName = {
+      box: (elem: Element) => {
+        const { scene, camera, controls } = makeScene(elem);
+        const geometry = new Three.BoxGeometry(1, 1, 1);
+        const material = new Three.MeshPhongMaterial({ color: "red" });
+        const mesh = new Three.Mesh(geometry, material);
+        scene.add(mesh);
+        return (time: number, rect: DOMRect) => {
+          mesh.rotation.y = time * 0.1;
+          camera.aspect = rect.width / rect.height;
+          camera.updateProjectionMatrix();
+          controls?.handleResize();
+          controls?.update();
+          renderer.render(scene, camera);
+        };
+      },
+      pyramid: (elem: Element) => {
+        const { scene, camera, controls } = makeScene(elem);
+        const geometry = new Three.SphereGeometry(0.8, 4, 2);
+        const material = new Three.MeshPhongMaterial({
+          color: "blue",
+          flatShading: true,
+        });
+        const mesh = new Three.Mesh(geometry, material);
+        scene.add(mesh);
+        return (time: number, rect: DOMRect) => {
+          mesh.rotation.y = time * 0.1;
+          camera.aspect = rect.width / rect.height;
+          camera.updateProjectionMatrix();
+          controls?.handleResize();
+          controls?.update();
+          renderer.render(scene, camera);
+        };
+      },
+    };
+    document.querySelectorAll("[data-diagram]").forEach(elem => {
+      const sceneName: any = (elem as HTMLElement).dataset.diagram;
+      const sceneRenderFunction =
+        sceneInitFunctionsByName[sceneName as "box" | "pyramid"](elem);
+      addScene(elem, sceneRenderFunction);
+    });
+
+    function addScene(elem: Element, fn: any) {
+      sceneElements.push({elem, fn});
+    }
 
     const render = (time: number) => {
       flag = false;
-      time *= 0.001
+      time *= 0.001;
       // handleResize()
+
+      renderer.setScissorTest(false);
+      renderer.clear(true, true);
+      renderer.setScissorTest(true);
 
       const transform = `translateY(${window.scrollY}px)`;
       renderer.domElement.style.transform = transform;
-      renderer.setScissorTest(false)
-      renderer.clear(true, true)
-      renderer.setScissorTest(true)
 
-      sceneInfo1.mesh && (sceneInfo1.mesh.rotation.y = time * 0.1)
-      sceneInfo2.mesh && (sceneInfo2.mesh.rotation.y = time * 0.1)
-      renderSceneInfo(sceneInfo1, renderer, canvas)
-      renderSceneInfo(sceneInfo2, renderer, canvas)
+      // sceneInfo1.mesh && (sceneInfo1.mesh.rotation.y = time * 0.1);
+      // sceneInfo2.mesh && (sceneInfo2.mesh.rotation.y = time * 0.1);
+      // renderSceneInfo(sceneInfo1, renderer, canvas);
+      // renderSceneInfo(sceneInfo2, renderer, canvas);
+      for (const {elem, fn} of sceneElements) {
+        // get the viewport relative position of this element
+        const rect = elem.getBoundingClientRect();
+        const {left, right, top, bottom, width, height} = rect;
+     
+        const isOffscreen =
+            bottom < 0 ||
+            top > renderer.domElement.clientHeight ||
+            right < 0 ||
+            left > renderer.domElement.clientWidth;
+     
+        if (!isOffscreen) {
+          const positiveYUpBottom = renderer.domElement.clientHeight - bottom;
+          renderer.setScissor(left, positiveYUpBottom, width, height);
+          renderer.setViewport(left, positiveYUpBottom, width, height);
+     
+          fn(time, rect);
+        }
+      }
       window.requestAnimationFrame(render);
     };
     window.requestAnimationFrame(render);
@@ -138,12 +215,12 @@ const MultipleScenes: React.FC = () => {
     <>
       <canvas ref={canvasRef} id="c" />
       <p>
-        <span id="box" className="diagram left"></span>I love boxes. Presents
-        come in boxes. When I find a new box I'm always excited to find out
-        what's inside.
+        <span data-diagram="box" className="diagram left"></span>I love boxes.
+        Presents come in boxes. When I find a new box I'm always excited to find
+        out what's inside.
       </p>
       <p>
-        <span id="pyramid" className="diagram right"></span>
+        <span data-diagram="pyramid" className="diagram right"></span>
         When I was a kid I dreamed of going on an expedition inside a pyramid
         and finding a undiscovered tomb full of mummies and treasure.
       </p>
